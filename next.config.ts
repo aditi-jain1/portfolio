2919f8f927afb7fc3b13c6bbaaab910fe462f7ae
Aditi Jain <aditijain@berkeley.edu>
2025-05-18 20:20:02 -0700

    linked github pages?

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
-import type { NextConfig } from "next";
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
+  basePath: '/portfolio',       // 👈 replace with actual repo name
+  assetPrefix: '/portfolio/',   // 👈 include trailing slash
 }
 
 module.exports = nextConfig
