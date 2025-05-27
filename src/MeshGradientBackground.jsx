0f9d14c0f106121b72ae304752b57a3f6fd2a0ae
Aditi Jain <aditijain@berkeley.edu>
2025-05-26 18:28:43 -0700

    build background

---
+++ src/MeshGradientBackground.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MeshGradientBackground.css';
+
+function MeshGradientBackground() {
+  return (
+    <div className="mesh-wrapper">
+      <div className="mesh-gradient-background" />
+      <h1 className="overlay-text typewriter">Aditi Jain</h1>
+    </div>
+  );
+}
+
+export default MeshGradientBackground;
